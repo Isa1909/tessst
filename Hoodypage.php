3d468596f11bf2159fbbcb3c8efe0c836a9ce3a8
Isa1909 <83549793+Isa1909@users.noreply.github.com>
2021-05-03 10:05:22 +0600

    Add files via upload

---
+++ Hoodypage.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HTML5 и CSS3 сайт</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <header>
+    <div id="logo" onclick="slowScroll ('#top')">
+      <span >Taktion</span>
+    </div>
+  
+
+  <div id="about">
+      
+      <a href="Hoodypage.php" href="#" title="Hoody" ">Hoody</a>
+      <a   href="#" onclick="slowScroll('#overview')" title="Joggers">Joggers</a>
+      <a href="#" onclick="slowScroll('#contacts')" title="T-shirts">T-shirts</a>
+    </div>
+  </header>
+
+  
+ 
+  
+  
+
+  <div id="overview">
+   <style>
+  h2 {
+    font-family: 'Suez One', serif;
+   }  h3 {
+    font-family: 'Suez One', serif;
+   } 
+  </style>
+    <h2>Hoody collection</h2>
+    
+	
+
+    <div class="img">
+
+      <a href="reg.php"><img src="https://admin.roadtothedream.com/images/products/sm/hoodyundercovercement_6054d205bc0bf4.38748640.jpg" alt=""></a>
+      <span >ХУДИ "UNDERCOVER"</span>
+	  <span >8990тг</span>
+	  
+
+	  
+    </div>
+    <div class="img">
+      <a href="reg.php"><img src="https://admin.roadtothedream.com/images/products/sm/rabotaem_603e3852210598.00805865.jpg" alt=""></a>
+      <span onclick>ХУДИ "Работаем" </span>
+	  <span onclick>9990тг </span>
+    </div>
+	
+	
+  </div>
+
+  
+
+  
+
+ 
+</body>
+</html>
---
+++ website123.html
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HTML5 и CSS3 сайт</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <header>
+    <div id="logo" onclick="slowScroll('#top')">
+      <span>Taktion</span>
+    </div>
+  
+
+  <div id="about">
+      
+      <a href="Hoodypage.php" href="#" title="Hoody" ">Hoody</a>
+      <a   href="#" onclick="slowScroll('#overview')" title="Joggers">Joggers</a>
+      <a href="#" onclick="slowScroll('#contacts')" title="T-shirts">T-shirts</a>
+    </div>
+	
+	
+  </header>
+
+  <div id="top">
+  <
+  <style>
+  h1 {
+    font-family: 'Suez One', serif;
+   }  h2 {
+    font-family: 'Suez One', serif;
+   } 
+  </style>
+    <h1  >Find your perfect </h1>
+	 <h2  > Sport clothes</h2>
+    
+  </div>
+
+ 
+  
+  
+
+  
+
+  
+
+  
+
+  
+</body>
+</html>
